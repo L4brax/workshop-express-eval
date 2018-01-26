21dc6d63e56862bfeb0d99846c1fed3551e1ae8d
L4brax <ant.hess2@gmail.com>
2018-01-26 15:03:06 +0100

    Express lib and server added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let chalk = require('chalk');
-
 const app = require('./src/app');
 
 // port
 const port = 3000;
 
 // start server here
-console.log(chalk.green('Hello web server'));
+app.listen(port, function () {
+  console.log(chalk.green('Hello web server'));
+});

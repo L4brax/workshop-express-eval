c1fe33e16655cf3c6dc3370607400f89f6e6500d
L4brax <ant.hess2@gmail.com>
2018-01-26 16:18:50 +0100

    Exercice 5

--- index.js
+++ index.js
@@ -7,8 +7,26 @@ const port = process.argv[2];
 
 // GET method on /bingo
 app.get('/bingo', function(req, res) {
+
+  // Reading numbers.txt
   fs.readFile('./src/numbers.txt', 'utf8', function(err, contents) {
-    res.send(contents.split('\n').join(', ').slice(0, -2));
+
+    // Parsing numbers.txt and transforming it
+    let numbers = contents.split('\n').join(', ').slice(0, -2);
+
+    // retrieving the parameter
+    let myNumbers = req.query.mynumbers;
+
+    // Sending the right answer
+    if (myNumbers == undefined){
+      res.send('The bingo game is already started and known numbers are '+numbers);
+    // Adding space after each comma to myNumbers to match the bingo numbers pattern string
+    } else if (myNumbers.replace(/,/g,', ') == numbers){
+      res.send('Bingo');
+    } else {
+      res.send(`The bingo game is already started, sorry your numbers
+      doesn't match with known numbers ${numbers} ; so you can not say Bingo`);
+    }
   });
 });
 

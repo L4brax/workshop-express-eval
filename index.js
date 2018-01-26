d6a0464b80174bf3173e239676fc3e66d5fada93
L4brax <ant.hess2@gmail.com>
2018-01-26 15:33:56 +0100

    exercice 4

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let chalk = require('chalk');
+const fs = require('fs');
 const app = require('./src/app');
 
 // port, specified in first argument
@@ -6,7 +7,9 @@ const port = process.argv[2];
 
 // GET method on /bingo
 app.get('/bingo', function(req, res) {
-  res.end('Bingo');
+  fs.readFile('./src/numbers.txt', 'utf8', function(err, contents) {
+    res.send(contents.split('\n').join(', ').slice(0, -2));
+  });
 });
 
 // start server here

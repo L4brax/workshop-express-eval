4aefa3b3d1b28ab971828aa9220f8ef4010c9394
L4brax <ant.hess2@gmail.com>
2018-01-26 15:15:15 +0100

    exercice 3

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = require('./src/app');
 // port, specified in first argument
 const port = process.argv[2];
 
+// GET method on /bingo
+app.get('/bingo', function(req, res) {
+  res.end('Bingo');
+});
+
 // start server here
 app.listen(port, function () {
   console.log(chalk.green('Hello web server'));

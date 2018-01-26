ef631316ad3a01ad9ae795b00956a2dd0b27fc6e
L4brax <ant.hess2@gmail.com>
2018-01-26 15:08:21 +0100

    Exercice 2 (sorry for the wrong messages on previous commits)

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let chalk = require('chalk');
 const app = require('./src/app');
 
-// port
+// port, specified in first argument
 const port = process.argv[2];
 
 // start server here
 app.listen(port, function () {
-  console.log(chalk.green('Hello web server !'));
+  console.log(chalk.green('Hello web server'));
 });
